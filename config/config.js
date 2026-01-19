2465baa28306b3667126b8c5a6c77df9b2d0da09
Jean-Pol <jpvw.ing@hotmail.com>
2026-01-19 16:12:36 +0100

    Sécurisation : suppression des valeurs par défaut sensibles dans config.js, ajout .gitignore

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 3004,
+  port: process.env.PORT,
   db: {
-    host: process.env.DB_HOST || '192.168.129.50',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Jpvw1953!',
-    database: process.env.DB_NAME || 'calendar_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   }
 };
